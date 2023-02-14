3f03ae48dcde20ef0f98ebf191d2f95fc18b8442
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-02-14 00:42:55 -0300

    chore: add google analytics

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePluginRadar } from 'vite-plugin-radar'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [
+      react(),
+      VitePluginRadar({
+        // Google Analytics tag injection
+        analytics: {
+          id: env.VITE_GOOGLE_ANALYTICS_ID
+        }
+      })
+    ]
+  }
 })
